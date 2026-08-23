d80517bccfc5262d11a24eb425d9926853ea8e6b
white-shark-ssw <houstools@gmail.com>
2026-08-24 03:07:00 +0800

    feat: add enhancer diagnostics interface

---
+++ ChatGPTEnhancer/Sources/Diagnostics/CEDiagnostics.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+NSString *CEDiagnosticsReport(UIView * _Nullable sourceView, NSString * _Nullable contextIdentifier);
+void CECopyDiagnostics(UIView * _Nullable sourceView, NSString * _Nullable contextIdentifier);
+
+NS_ASSUME_NONNULL_END
