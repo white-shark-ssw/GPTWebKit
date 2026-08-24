f3c8c71a1f22547acbfc02e1fc295e00996e0aca
white-shark-ssw <houstools@gmail.com>
2026-08-25 04:37:23 +0800

    feat: add targeted in-place recovery probe

---
+++ ChatGPTEnhancer/Sources/Diagnostics/CEInPlaceRecoveryProbe.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+void CEInPlaceRecoveryProbe(NSString *reason);
+NSString *CEInPlaceRecoveryProbeSnapshot(void);
+
+NS_ASSUME_NONNULL_END
