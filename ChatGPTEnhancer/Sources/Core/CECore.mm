21cb51bf3b8b5678b7affbb33a00e8f00960a0ea
white-shark-ssw <houstools@gmail.com>
2026-08-25 05:03:13 +0800

    chore: bump enhancer to alpha30 stream probe

--- ChatGPTEnhancer/Sources/Core/CECore.mm
+++ ChatGPTEnhancer/Sources/Core/CECore.mm
@@ -2,7 +2,7 @@
 #import <objc/runtime.h>
 
 NSString * const CEBundleIdentifier = @"com.openai.chat";
-NSString * const CEVersion = @"0.1.0-alpha29-native-probe";
+NSString * const CEVersion = @"0.1.0-alpha30-stream-state-probe";
 NSString * const CEConversationContextDidChangeNotification = @"ChatGPTEnhancer.ConversationContextDidChange";
 NSString * const CENetworkTemplateDidChangeNotification = @"ChatGPTEnhancer.NetworkTemplateDidChange";
 NSString * const CECatalogDidChangeNotification = @"ChatGPTEnhancer.CatalogDidChange";
