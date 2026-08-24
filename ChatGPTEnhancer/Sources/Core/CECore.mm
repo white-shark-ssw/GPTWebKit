4df9fd3d3decf25f968352b6035770b0de9ee96b
white-shark-ssw <houstools@gmail.com>
2026-08-25 02:03:32 +0800

    chore: bump enhancer to alpha26 diagnostic

--- ChatGPTEnhancer/Sources/Core/CECore.mm
+++ ChatGPTEnhancer/Sources/Core/CECore.mm
@@ -2,7 +2,7 @@
 #import <objc/runtime.h>
 
 NSString * const CEBundleIdentifier = @"com.openai.chat";
-NSString * const CEVersion = @"0.1.0-alpha25-diagnostic";
+NSString * const CEVersion = @"0.1.0-alpha26-diagnostic";
 NSString * const CEConversationContextDidChangeNotification = @"ChatGPTEnhancer.ConversationContextDidChange";
 NSString * const CENetworkTemplateDidChangeNotification = @"ChatGPTEnhancer.NetworkTemplateDidChange";
 NSString * const CECatalogDidChangeNotification = @"ChatGPTEnhancer.CatalogDidChange";
