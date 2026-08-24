2924a88d40d2933a5160848c6ad51667afbefedc
white-shark-ssw <houstools@gmail.com>
2026-08-25 01:17:29 +0800

    diag: expose active conversation runtime tracing

--- ChatGPTEnhancer/Sources/Diagnostics/CEDiagnostics.h
+++ ChatGPTEnhancer/Sources/Diagnostics/CEDiagnostics.h
@@ -5,5 +5,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 NSString *CEDiagnosticsReport(UIView * _Nullable sourceView, NSString * _Nullable contextIdentifier);
 void CECopyDiagnostics(UIView * _Nullable sourceView, NSString * _Nullable contextIdentifier);
+void CEInstallActiveConversationDiagnostics(void);
 
 NS_ASSUME_NONNULL_END
