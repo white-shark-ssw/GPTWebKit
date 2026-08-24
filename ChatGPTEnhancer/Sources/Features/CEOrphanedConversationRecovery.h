ba16f20b78344007bbf4309573f244e890ce6964
white-shark-ssw <houstools@gmail.com>
2026-08-25 00:01:17 +0800

    feat: expose lightweight current conversation refresh

--- ChatGPTEnhancer/Sources/Features/CEOrphanedConversationRecovery.h
+++ ChatGPTEnhancer/Sources/Features/CEOrphanedConversationRecovery.h
@@ -1,5 +1,6 @@
 #import <Foundation/Foundation.h>
 
 BOOL CEOrphanReselectConversation(NSString *conversationID);
+void CEOrphanRefreshConversation(NSString *conversationID, void (^completion)(BOOL success));
 void CEOrphanForceReloadConversation(NSString *conversationID, void (^completion)(BOOL success));
 NSString *CEOrphanedConversationRecoveryDiagnosticsSnapshot(void);
