9f158cef552001d2fcabd3549d2f2750503d1373
white-shark-ssw <houstools@gmail.com>
2026-08-24 23:36:03 +0800

    diagnostics: expose reload snapshot

--- ChatGPTEnhancer/Sources/Features/CEOrphanedConversationRecovery.h
+++ ChatGPTEnhancer/Sources/Features/CEOrphanedConversationRecovery.h
@@ -2,3 +2,4 @@
 
 BOOL CEOrphanReselectConversation(NSString *conversationID);
 void CEOrphanForceReloadConversation(NSString *conversationID, void (^completion)(BOOL success));
+NSString *CEOrphanedConversationRecoveryDiagnosticsSnapshot(void);
