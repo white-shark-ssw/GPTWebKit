7c4e0326add7e87dc43d4812e37426ad2d19487f
white-shark-ssw <houstools@gmail.com>
2026-08-24 18:02:30 +0800

    chore: bump ChatGPTEnhancer to alpha15

--- ChatGPTEnhancer/build.sh
+++ ChatGPTEnhancer/build.sh
@@ -28,6 +28,6 @@ codesign -f -s - "$BUILD/ChatGPTEnhancer.dylib"
 cp "$BUILD/ChatGPTEnhancer.dylib" "$BUILD/package/"
 cp "$ROOT/Support/ChatGPTEnhancer.plist" "$BUILD/package/"
 cp "$ROOT/README.md" "$BUILD/package/README.txt"
-(cd "$BUILD/package" && /usr/bin/zip -9 -r "../ChatGPTEnhancer-0.1.0-alpha14.zip" .)
+(cd "$BUILD/package" && /usr/bin/zip -9 -r "../ChatGPTEnhancer-0.1.0-alpha15.zip" .)
 file "$BUILD/ChatGPTEnhancer.dylib"
-ls -lh "$BUILD/ChatGPTEnhancer.dylib" "$BUILD/ChatGPTEnhancer-0.1.0-alpha14.zip"
+ls -lh "$BUILD/ChatGPTEnhancer.dylib" "$BUILD/ChatGPTEnhancer-0.1.0-alpha15.zip"
