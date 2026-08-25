ef7afa7f9714380cc9c566f7acd8dd9242e9485c
white-shark-ssw <houstools@gmail.com>
2026-08-25 19:11:43 +0800

    chore: bump enhancer to alpha39 reload stability

--- ChatGPTEnhancer/Sources/Core/CECore.mm
+++ ChatGPTEnhancer/Sources/Core/CECore.mm
@@ -2,7 +2,7 @@
 #import <objc/runtime.h>
 
 NSString * const CEBundleIdentifier = @"com.openai.chat";
-NSString * const CEVersion = @"0.1.0-alpha38-ui-usage-stability";
+NSString * const CEVersion = @"0.1.0-alpha39-reload-stability";
 NSString * const CEConversationContextDidChangeNotification = @"ChatGPTEnhancer.ConversationContextDidChange";
 NSString * const CENetworkTemplateDidChangeNotification = @"ChatGPTEnhancer.NetworkTemplateDidChange";
 NSString * const CECatalogDidChangeNotification = @"ChatGPTEnhancer.CatalogDidChange";
