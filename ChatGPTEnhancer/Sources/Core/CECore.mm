89474edfc5869608f35fd09e2765a994a1c25508
white-shark-ssw <houstools@gmail.com>
2026-08-25 21:36:40 +0800

    chore: bump enhancer candidate to alpha41

--- ChatGPTEnhancer/Sources/Core/CECore.mm
+++ ChatGPTEnhancer/Sources/Core/CECore.mm
@@ -2,7 +2,7 @@
 #import <objc/runtime.h>
 
 NSString * const CEBundleIdentifier = @"com.openai.chat";
-NSString * const CEVersion = @"0.1.0-alpha40-conversation-recognition";
+NSString * const CEVersion = @"0.1.0-alpha41-project-conversation-title";
 NSString * const CEConversationContextDidChangeNotification = @"ChatGPTEnhancer.ConversationContextDidChange";
 NSString * const CENetworkTemplateDidChangeNotification = @"ChatGPTEnhancer.NetworkTemplateDidChange";
 NSString * const CECatalogDidChangeNotification = @"ChatGPTEnhancer.CatalogDidChange";
