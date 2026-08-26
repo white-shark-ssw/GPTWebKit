87efdc63a8810dd47aafb94e8d58379369aa3439
white-shark-ssw <houstools@gmail.com>
2026-08-26 14:22:41 +0800

    build: set alpha44 current conversation guard version

--- ChatGPTEnhancer/Sources/Core/CECore.mm
+++ ChatGPTEnhancer/Sources/Core/CECore.mm
@@ -2,7 +2,7 @@
 #import <objc/runtime.h>
 
 NSString * const CEBundleIdentifier = @"com.openai.chat";
-NSString * const CEVersion = @"0.1.0-alpha42-project-conversation-title";
+NSString * const CEVersion = @"0.1.0-alpha44-current-conversation-guard";
 NSString * const CEConversationContextDidChangeNotification = @"ChatGPTEnhancer.ConversationContextDidChange";
 NSString * const CENetworkTemplateDidChangeNotification = @"ChatGPTEnhancer.NetworkTemplateDidChange";
 NSString * const CECatalogDidChangeNotification = @"ChatGPTEnhancer.CatalogDidChange";
