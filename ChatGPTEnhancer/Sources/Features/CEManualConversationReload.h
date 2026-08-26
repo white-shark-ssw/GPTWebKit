977435b1776e5821b8b4057a551bfad547f026b8
white-shark-ssw <houstools@gmail.com>
2026-08-27 03:17:39 +0800

    fix: distinguish refresh request from reload completion

--- ChatGPTEnhancer/Sources/Features/CEManualConversationReload.h
+++ ChatGPTEnhancer/Sources/Features/CEManualConversationReload.h
@@ -3,5 +3,6 @@
 NS_ASSUME_NONNULL_BEGIN
 
 void CEManualReloadConversationID(NSString *conversationID);
+void CEManualReloadConversationIDWithStartMessage(NSString *conversationID, NSString *startMessage);
 
 NS_ASSUME_NONNULL_END
