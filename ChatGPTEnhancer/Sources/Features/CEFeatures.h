915dbaf2c9853311e45b6ef20085c1184a87daeb
white-shark-ssw <houstools@gmail.com>
2026-08-26 20:26:29 +0800

    feat: expose exact conversation actions

--- ChatGPTEnhancer/Sources/Features/CEFeatures.h
+++ ChatGPTEnhancer/Sources/Features/CEFeatures.h
@@ -7,7 +7,10 @@ NS_ASSUME_NONNULL_BEGIN
 @interface CEFeatures : NSObject
 + (void)exportCandidates:(NSArray<CEConversationRecord *> *)candidates fromContextMenu:(BOOL)fromContextMenu;
 + (void)exportRecord:(CEConversationRecord *)record requireConfirmation:(BOOL)requireConfirmation;
++ (void)exportConversationID:(NSString *)conversationID title:(nullable NSString *)title;
 + (void)renameCandidates:(NSArray<CEConversationRecord *> *)candidates sourceView:(nullable UIView *)sourceView;
++ (void)pullLatestConversationID:(NSString *)conversationID;
++ (void)reloadConversationID:(NSString *)conversationID;
 + (void)pullLatestCurrentConversation;
 + (void)reloadCurrentConversation;
 @end
