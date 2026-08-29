559910a12d6dac51f0a9b188f38c0ff9562b6d1b
white-shark-ssw <houstools@gmail.com>
2026-08-30 04:46:22 +0800

    build: package alpha60 runtime image map

--- ChatGPTEnhancer/build.sh
+++ ChatGPTEnhancer/build.sh
@@ -28,6 +28,6 @@ codesign -f -s - "$BUILD/ChatGPTEnhancer.dylib"
 cp "$BUILD/ChatGPTEnhancer.dylib" "$BUILD/package/"
 cp "$ROOT/Support/ChatGPTEnhancer.plist" "$BUILD/package/ChatGPTEnhancer.plist"
 cp "$ROOT/README.md" "$BUILD/package/README.txt"
-(cd "$BUILD/package" && /usr/bin/zip -9 -r "../ChatGPTEnhancer-0.1.0-alpha59-runtime-owner-map.zip" .)
+(cd "$BUILD/package" && /usr/bin/zip -9 -r "../ChatGPTEnhancer-0.1.0-alpha60-runtime-image-map.zip" .)
 file "$BUILD/ChatGPTEnhancer.dylib"
-ls -lh "$BUILD/ChatGPTEnhancer.dylib" "$BUILD/ChatGPTEnhancer-0.1.0-alpha59-runtime-owner-map.zip"
+ls -lh "$BUILD/ChatGPTEnhancer.dylib" "$BUILD/ChatGPTEnhancer-0.1.0-alpha60-runtime-image-map.zip"
